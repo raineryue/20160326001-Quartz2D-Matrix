953b3889f51d061cd554b26b18c9a1741d841702
raineryue <it_jinlei.yue@163.com>
2016-03-26 23:20:48 +0800

    Initial Commit

---
+++ 20160326001-Quartz2D-Matrix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160326001-Quartz2D-Matrix
+//
+//  Created by Rainer on 16/3/26.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20160326001-Quartz2D-Matrix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160326001-Quartz2D-Matrix
+//
+//  Created by Rainer on 16/3/26.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
